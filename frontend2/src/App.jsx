360ceb0a59dc9d23d274e79d93cd30482e7209e1
sheikhmuhammadzain <zaingee320@gmail.com>
2024-12-18 12:48:58 +0500

    asdf

---
+++ frontend2/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+function App() {
+  async function fetchData() {
+    const response = await axios.get("http://127.0.0.1:8000/api/students/");
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
